b82f8b22e724e9b96cf51069756316c434997c77
Marjatta Wallinheimo <marjatta.wallinheimo@gmail.com>
2025-02-18 11:54:00 +0200

    Tehtävä 1.6: Unicafe step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,20 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.name} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p><strong>Total exercises: {total}</strong></p>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import { useState } from 'react';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Anna palautetta</h1>
+      <button onClick={() => setGood(good + 1)}>Hyvä</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutraali</button>
+      <button onClick={() => setBad(bad + 1)}>Huono</button>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   );
 };
